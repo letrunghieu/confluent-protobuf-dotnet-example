8bb4e5f66329d1c923497e149e544fa0acf2c632
HieuLT2 <hieult2@batdongsan.com.vn>
2023-11-23 16:14:40 +0700

    Produce protobuf message

---
+++ ConfluentProtobufDotnetExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.Extensions.Configuration;
+using ConfluentProtobufDotnetExample;
+using Google.Protobuf;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("./appsettings.json")
+    .AddUserSecrets(typeof(Program).Assembly)
+    .Build();
+    
+var producerConfig = configuration.GetSection("Producer").Get<ProducerConfig>().ThrowIfContainsNonUserConfigurable();
+var schemaRegistryConfig = configuration.GetSection("SchemaRegistry").Get<SchemaRegistryConfig>();
+var topicName = configuration.GetValue<string>("General:TopicName");
+var sampleSize = configuration.GetValue<int>("General:SampleSize");
+
+using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+using var producer = new ProducerBuilder<Null, CustomerEvent>(producerConfig)
+    .SetValueSerializer(new ProtobufSerializer<CustomerEvent>(schemaRegistry))
+    .Build();
+
+var formatter = new JsonFormatter(JsonFormatter.Settings.Default.WithIndentation());
+for (var i = 0; i < sampleSize; i++)
+{
+    var messsage = new CustomerEvent()
+    {
+        CustomerId = i,
+        EventName = i.ToString(),
+        Data = i.ToString() + " data",
+        CreatedDate = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow),
+    };
+    Console.WriteLine(formatter.Format(messsage));
+    await producer.ProduceAsync(topicName, new Message<Null, CustomerEvent>() { Value = messsage });
+}
